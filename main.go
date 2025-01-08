d6492156a8806fe6e2a0d3e4acbb0b39075cc39e
William Santos <w.santos.gg@gmail.com>
2025-01-08 17:32:35 +0800

    Added OpenGL

--- main.go
+++ main.go
@@ -1,5 +1,65 @@
 package main
 
+import (
+	"log"
+	"runtime"
+
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/glfw/v3.2/glfw"
+)
+
 func main() {
-	println("hello world")
+	runtime.LockOSThread()
+
+	window := initGlfw()
+	program := initOpenGL()
+
+	defer glfw.Terminate()
+
+	for !window.ShouldClose() {
+		draw(window, program)
+	}
+}
+
+func draw(window *glfw.Window, program uint32) {
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+	gl.UseProgram(program)
+
+	glfw.PollEvents()
+	window.SwapBuffers()
+}
+
+func initOpenGL() uint32 {
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+
+	version := gl.GoStr(gl.GetString(gl.VERSION))
+	log.Println("OpenGL Version", version)
+
+	prog := gl.CreateProgram()
+	gl.LinkProgram(prog)
+
+	return prog
+}
+
+func initGlfw() *glfw.Window {
+	if err := glfw.Init(); err != nil {
+		panic(err)
+	}
+
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	window, err := glfw.CreateWindow(800, 800, "Snowtime", nil, nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+	return window
 }
